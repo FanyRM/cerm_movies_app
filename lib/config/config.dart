0aec0c84d4c8c2f334cfef355f963efddedeb195
FanyRM <celeste.e.ramirez.m@gmail.com>
2026-06-19 17:40:57 -0600

    fix: Construccion del detalle de la pelicula

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,3 +1,4 @@
 export 'package:cerm_movies_app/config/constants/environment.dart';
 export 'package:cerm_movies_app/config/theme/app_theme.dart';
 export 'package:cerm_movies_app/config/router/app_router.dart';
+export 'package:cerm_movies_app/config/helpers/human_formats.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:cerm_movies_app/config/config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
 //import 'package:cerm_movies_app/config/theme/app_theme.dart';
 //import 'package:cerm_movies_app/presentation/screens/screens.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
-  print(dotenv.env['MOVIEDB_KEY']);
+
   runApp(ProviderScope(child: MainApp()));
 }
 
@@ -16,6 +18,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    initializeDateFormatting();
+
     return MaterialApp.router(
       // home: HomeScreen(),
       routerConfig: appRouter,
